eebdad963847870c691f460a83e2bd4dd413dce9
José Nascimento <joseeantonioo2000@gmail.com>
2024-07-04 13:16:27 -0300

    feat: inclusion of input for lenght of charecters

--- src/components/password_generator/index.jsx
+++ src/components/password_generator/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { Button, Card, CardActions, CardContent, Stack, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Stack, TextField, Typography } from "@mui/material";
 import copy from "copy-to-clipboard";
 
 export default function PasswordGenerator() {
     const [passwordValue, setPasswordValue] = useState("");
     const [textGenerator, setTextGenerator] = useState("Gerar!");
     const [textCopy, setTextCopy] = useState("Copiar");
+    const [passwordSize, setPasswordSize] = useState(12)
 
     const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
-    const passwordLength = 12;
+    const passwordLength = passwordSize;
     function generateRandomPassword(length) {
         let result = '';
         for (let i = 0; i < length; i++) {
@@ -33,6 +34,13 @@ export default function PasswordGenerator() {
             <Card sx={{ width: 700, height: 350, display: 'flex', justifyContent: 'center', alignItems: 'center', background: "#3c3c3c" }}>
                 <CardContent>
                     <Typography gutterBottom variant="h4" sx={{ color: "#fff" }}>Gerador de senhas</Typography>
+                    <TextField 
+                    sx={{display:'flex', justifyContent: "center", background: "#fff"}}
+                    required
+                    label="Tamanho"
+                    value={passwordSize}
+                    size="small"
+                    onChange={(ev) => setPasswordSize(ev.target.value)}/>
                     <CardActions sx={{ display: 'flex', justifyContent: "center", gap: 2, mb: 2 }}>
                         <Button sx={{ background: "#181818", color: "#fff" }} onClick={Random}>{textGenerator}</Button>
                         <Button sx={{ background: "#181818", color: "#fff" }} onClick={Copy}>{textCopy}</Button>
