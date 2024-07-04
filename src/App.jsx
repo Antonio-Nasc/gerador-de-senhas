4131a018ec25b5b09bc1fac81a1253ca76532769
José Nascimento <joseeantonioo2000@gmail.com>
2024-07-04 10:40:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import PasswordGenerator from "./components/password_generator"
+
+function App() {
+
+  return (
+    <>
+      <PasswordGenerator />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/password_generator/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, Card, CardActions, CardContent, Stack, Typography } from "@mui/material";
+import copy from "copy-to-clipboard";
+
+export default function PasswordGenerator() {
+    const [passwordValue, setPasswordValue] = useState("");
+    const [textGenerator, setTextGenerator] = useState("Gerar!");
+    const [textCopy, setTextCopy] = useState("Copiar");
+
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    const passwordLength = 12;
+    function generateRandomPassword(length) {
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            result += charset[randomIndex];
+        }
+        return result;
+    }
+    function Random() {
+        const newPassword = generateRandomPassword(passwordLength);
+        setPasswordValue(newPassword);
+        setTextCopy("Copiar");
+    }
+
+    function Copy() {
+        copy(passwordValue);
+        setTextCopy("Copiado!");
+    }
+
+    return (
+        <Stack sx={{ height: "100vh", background: "#181818", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Card sx={{ width: 700, height: 350, display: 'flex', justifyContent: 'center', alignItems: 'center', background: "#3c3c3c" }}>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" sx={{ color: "#fff" }}>Gerador de senhas</Typography>
+                    <CardActions sx={{ display: 'flex', justifyContent: "center", gap: 2, mb: 2 }}>
+                        <Button sx={{ background: "#181818", color: "#fff" }} onClick={Random}>{textGenerator}</Button>
+                        <Button sx={{ background: "#181818", color: "#fff" }} onClick={Copy}>{textCopy}</Button>
+                    </CardActions>
+                    <Typography sx={{textAlign: "center", color: "#fff"}}>{passwordValue}</Typography>
+                </CardContent>
+            </Card>
+        </Stack>
+    );
+}
